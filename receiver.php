86b2d67c4a6f1de9f0c655e6e16fdd1ce6ee43ce
Søren Louv-Jansen <sl@konscript.com>
2011-06-07 18:11:47 +0200

    Fixed situation when payload not received

--- receiver.php
+++ receiver.php
@@ -6,15 +6,15 @@ include("inc/deploy.class.php");
 
 //Receive and decode the json payload string
 if($_SERVER["REMOTE_ADDR"] == "127.0.0.1"){
-    $payload = file_get_contents("./payload"); 
+    $payload = file_get_contents("./payload");  //localhost
 }else{
-    $payload = $_REQUEST['payload'];
+    if(isset($_REQUEST['payload'])){
+        $payload = $_REQUEST['payload'];    //payload received
+    }else{
+        echo "No payload was received!";    //payload not received
+        exit();
+    }
 }
-
-if(!isset($payload)){
-    echo "No payload was received!";
-    exit();
-}   
 
 $deploy = new deploy($payload);
 
